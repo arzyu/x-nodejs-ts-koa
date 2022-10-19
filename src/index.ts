d7d43a0a0d7b391c553afe55dee0d6f22fb0ec49
arzyu <arzyu@live.cn>
2022-10-19 17:13:52 +0800

    add @koa/router, koa-logger

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import Koa from "koa";
+import logger from "koa-logger";
+
+import { router } from "./router";
 
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  ctx.body = "Hello Koa";
-  await next();
-});
+app.use(logger());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`\nstart server: http://0.0.0.0:${port}`);
+  console.log(`>> Server: http://0.0.0.0:${port}`);
 });
